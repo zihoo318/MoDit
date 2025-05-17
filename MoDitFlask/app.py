4075a0e75026e2dc3687b76e6fc3bf566154b637
zihoo318 <zihoo318@gmail.com>
2025-05-17 17:56:01 +0900

    과제 관리(메뉴포함) 완성

--- MoDitFlask/app.py
+++ MoDitFlask/app.py
@@ -3,7 +3,8 @@ from flask import Flask
 from routes.stt_route import stt_bp
 from routes.ocr_route import ocr_bp
 from routes.summary_route import summary_bp
-from routes.task_route import task_route 
+from routes.task_route import task_bp
+
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -14,7 +15,8 @@ CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 app.register_blueprint(stt_bp)
 app.register_blueprint(ocr_bp)
 app.register_blueprint(summary_bp)
-app.register_blueprint(task_route)
+app.register_blueprint(task_bp)
+app.register_blueprint(note_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
